371517ece886e94d716ca4428e22c7cf7310b554
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-12-11 17:08:10 -0300

    Chore: Adiciona Tailwind Css ao Projeto

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      background: "#282832",
+      primary: "#77181e",
+      secondary: "secondary",
+      neutral: "#dee7e7",
+      "base-100": "base-100",
+      success: "success",
+    },
+    size: {
+      image: "4rem",
+    },
+  },
+  plugins: [],
+};
